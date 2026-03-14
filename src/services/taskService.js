bc014d20e57c0a3aacf57f950a9df087728ab602
otmeza <otmeza@mutualser.org>
2026-03-14 17:14:12 -0500

    add nginx.conf

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:9090/api/tasks';
+const API_URL = '/api/tasks';
 
 export const getTasks = async () => {
   const response = await axios.get(API_URL);
